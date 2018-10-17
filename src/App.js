1c38698cf577db6424a7baf4e214221a324db6a2
Dinesh Ramasamy <dinesh.vgp@gmail.com>
2018-10-17 09:09:16 +0530

    App initial setup and code cleanups

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'antd/dist/antd.css';
+import { Layout } from 'antd';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const { Header, Footer, Sider, Content } = Layout;
+
+const App = () => (
+  <div className="App">
+    <Layout>
+      <Header>Header</Header>
+      <Content>Content</Content>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Content>Content</Content>
+        <Sider>Sider</Sider>
+      </Layout>
+      <Footer>Footer</Footer>
+    </Layout>
+  </div>
+);
 
 export default App;
